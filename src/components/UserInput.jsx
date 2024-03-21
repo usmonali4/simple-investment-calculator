01704e0cdc97c2cba4f5acf94355044443aee407
Usmon <usmonpahlavonov@gmail.com>
2024-03-21 14:20:12 +0900

    Added Event Handler for User Input

--- src/components/UserInput.jsx
+++ src/components/UserInput.jsx
@@ -1,23 +1,49 @@
+import { useState } from "react"
+
 export default function UserInput() {
+    const [userInput, setUserInput] = useState({
+        initialInvestment: 10000,
+        annualInvestment: 1200,
+        expectedReturn: 6,
+        duration: 10,
+      });
+
+    function handleChange(inputIdentifier, newValue){
+        setUserInput((prevInput) => {
+            return {
+                ...prevInput,
+                [inputIdentifier]: Number(newValue),
+            };
+        })
+    }
+
     return <section id="user-input">
         <div className="input-group">
             <p>
                 <label>Initial Investment</label>
-                <input type="number"/>
+                <input type="number" 
+                value={userInput.initialInvestment}
+                onChange={(e) => handleChange("initialInvestment", e.target.value)}/>
             </p>
             <p>
                 <label>Annual Investment</label>
-                <input type="number"/>
+                <input type="number" 
+                value={userInput.annualInvestment}
+                onChange={(e) => handleChange("annualInvestment", e.target.value)}/>
             </p>
         </div>
         <div className="input-group">
             <p>
                 <label>Expected Return</label>
-                <input type="number"/>
+                <input type="number" 
+                value={userInput.expectedReturn}
+                onChange={(e) => handleChange("expectedReturn", e.target.value)}/>
             </p>
             <p>
                 <label>Duration</label>
-                <input type="number"/>
+                <input type="number" 
+                value={userInput.duration}
+                onChange={(e) => handleChange("duration", e.target.value)}/>
             </p>
         </div>
     </section>
