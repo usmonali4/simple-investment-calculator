a30ded6a7e3e27273c8a946d021709d75a83d1c2
Usmon <usmonpahlavonov@gmail.com>
2024-03-21 13:51:35 +0900

    Updated README file and added UserInput component with some styles

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # simple-investment-calculator
+Usage:
+1. copy repository to your IDE
+2. Install packages by running the code: npm install
+3. See result by running: nmp run dev
---
+++ src/components/UserInput.jsx
@@ -0,0 +1,24 @@
+export default function UserInput() {
+    return <section id="user-input">
+        <div className="input-group">
+            <p>
+                <label>Initial Investment</label>
+                <input type="number"/>
+            </p>
+            <p>
+                <label>Annual Investment</label>
+                <input type="number"/>
+            </p>
+        </div>
+        <div className="input-group">
+            <p>
+                <label>Expected Return</label>
+                <input type="number"/>
+            </p>
+            <p>
+                <label>Duration</label>
+                <input type="number"/>
+            </p>
+        </div>
+    </section>
+}
